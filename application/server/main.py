76bd3765f7917065dafa8447213e7dfc5fccf685
Joaquin <joaquinegocheagaramos@gmail.com>
2021-09-06 14:58:55 -0500

    link

--- application/server/main.py
+++ application/server/main.py
@@ -1,32 +1,50 @@
 import uvicorn
-from fastapi import FastAPI, File, UploadFile
+from fastapi import (FastAPI, File, UploadFile)
 from starlette.responses import RedirectResponse
-
+from tensorflow.python.keras.preprocessing import image as imgx
+import requests
+from PIL import Image
 from application.components import predict, read_imagefile
 from application.schema import Symptom
 from application.components.prediction import symptom_check
-
+from googletrans import Translator, constants
+from pprint import pprint
 app_desc = """<h2>Try this app by uploading any image with `predict/image`</h2>
-<h2>Try Covid symptom checker api - it is just a learning app demo</h2>
-<br>by Aniket Maurya"""
+<h2>Analize photos</h2>
+<br>Template by Aniket Maurya, new version by Joaquin Egocheaga"""
 
-app = FastAPI(title='Tensorflow FastAPI Starter Pack', description=app_desc)
+app = FastAPI(title='Comparizy  ,  Tensorflow FastAPI ', description=app_desc)
 
-
+translator = Translator()
 @app.get("/", include_in_schema=False)
 async def index():
     return RedirectResponse(url="/docs")
 
 
 @app.post("/predict/image")
-async def predict_api(file: UploadFile = File(...)):
-    extension = file.filename.split(".")[-1] in ("jpg", "jpeg", "png")
+async def predict_api(filename):
+    extension = filename.split(".")[-1] in ("jpg", "jpeg", "png")
+ 
+    print(filename)
+    print(extension)
     if not extension:
         return "Image must be jpg or png format!"
-    image = read_imagefile(await file.read())
+
+    image = Image.open(requests.get(filename, stream=True).raw)
+    
+         
     prediction = predict(image)
 
-    return prediction
+    clase=prediction[0]['class']
+    clase=clase.replace("_", " ")
+    print(clase)
+    
+    print("X")
+    translation = translator.translate(clase, "es") 
+    translation=translation.text
+    print(translation)
+    
+    return translation
 
 
 @app.post("/api/covid-symptom-check")
